e16ace4a8c2569c5050c499a7671b528b7c07d3a
GlebZhidovich <gzhidovich@gmail.com>
2021-09-13 19:43:18 +0300

    feat: add logic for app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { TitlePipe } from './pipes/title.pile';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './components/app/app.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent,
+    TitlePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CountService } from './../../services/count.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  countForm: FormGroup;
+  data: any;
+
+  constructor(private countService: CountService) {
+    this.countForm = new FormGroup({
+      distance: new FormControl('10', [Validators.required, Validators.pattern("^[0-9]+$")]),
+      age: new FormControl('10', [Validators.required, Validators.pattern("^[0-9]+$")]),
+      weight: new FormControl('25', [Validators.required, Validators.pattern("^[0-9]+$")])
+    });
+  }
+
+  onSubmit(): void {
+    const value: { [k: string]: string } = this.countForm.value;
+    const values: [string, number][] = Object.entries(value).map(([name, value]) => [name, parseInt(value, 10)])
+    const { distance, age, weight } = getObjromArr(values);
+    const result = this.countService.getPrice({ distance, age, weight });
+    this.data = result;
+  }
+
+}
+
+function getObjromArr(arr: [string, any][]): { [k: string]: any } {
+  const result: { [k: string]: any } = {};
+  arr.forEach(([name, value]) => {
+    result[name] = value;
+  })
+  return result;
 }
